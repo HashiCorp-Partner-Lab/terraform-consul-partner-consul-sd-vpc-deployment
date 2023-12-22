4364e1d4c6b3213c913fe8d379cd5c66e5c33115
Tyler Allen <tyler.allen@hashicorp.com>
2023-12-22 21:33:07 +0000

    I like it with spaces better

--- variables.tf
+++ variables.tf
@@ -5,50 +5,60 @@ variable "hvn_id" {
   type        = string
   default     = "partner-hvn"
 }
+
 variable "cluster_id" {
   description = "The ID of the HCP Consul cluster."
   type        = string
   default     = "partner-hcp-consul"
 }
+
 variable "region" {
   description = "The region of the HCP HVN and Consul cluster."
   type        = string
   default     = "us-west-2"
 }
+
 variable "cloud_provider" {
   description = "The cloud provider of the HCP HVN and Consul cluster."
   type        = string
   default     = "aws"
 }
+
 variable "peering_id" {
   description = "The ID of the HCP peering connection."
   type        = string
   default     = "partner-peering"
 }
+
 variable "hpl_hcp_hvn_id" {
   description = "The ID of the HCP HVN route."
   type        = string
   default     = "partner-hvn-route"
 }
+
 variable "hcp_client_id" {
   description = "Your HashiCorp Cloud Platform client ID."
   type = string
   default = "value"
 }
+
 variable "hcp_client_secret" {
   description = "The client secret key associated with your HCP account."
   type = string
   default = "value"
 }
+
 variable "hpl_hcp_project_id" {
   description = "The project key for your HCP account."
   type = string
 }
+
 variable "aws_access_key" {
   description = "AWS Access Key ID for the account to be peered to"
   type = string
   sensitive = false
 }
+
 variable "aws_secret_key" {
   description = "AWS Secret Access Key for the account to be peered to"
   type = string
