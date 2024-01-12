996e484f28dac603809b15061dc9d68e9d1f9621
Benjamin Holmes <ben.holmes@hashicorp.com>
2024-01-12 20:46:33 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -20,10 +20,12 @@ output "consul_config_file" {
 
 output "consul_private_endpoint_url" {
   value = hcp_consul_cluster.partner_hcp.consul_private_endpoint_url
+  description = "the private URL for the consul cluster"
 }
 
 output "consul_public_endpoint_url" {
   value = hcp_consul_cluster.partner_hcp.consul_public_endpoint_url
+  description = "the public URL for the consul cluster"
 }
 
 output "consul_root_token_accessor_id" {
@@ -31,8 +33,8 @@ output "consul_root_token_accessor_id" {
 }
 
 output "consul_root_token_secret_id" {
-  value = hcp_consul_cluster.partner_hcp.consul_root_token_secret_id
-  sensitive = true
+  value = nonsensitive(hcp_consul_cluster.partner_hcp.consul_root_token_secret_id)
+  description = "the consul root token"
 }
 
 output "consul_cluster_id" {
